bf4103656032df632d8df4127af490ffbc472162
Lucas Aragão Tavares Mota <136651806+Lucasholt124@users.noreply.github.com>
2024-10-29 17:24:39 -0300

    Atualização
    Atualizando cards

--- src/Components/sales-summary/SalesSummaryCard.tsx
+++ src/Components/sales-summary/SalesSummaryCard.tsx
@@ -1,11 +1,14 @@
-
 import './styles.css';
+import icon04 from '../../assets/icon04.svg';
 
-const salesSummaryCard = () => {
+const SalesSummaryCard = () => {
   return (
     <div className="sales-summary-card base-card">
+      <img src={icon04} alt="Icon" className="sales-summary-card-icon" />
+      <h3 className="sales-summary-card-value">534.00</h3>
+      <span className="sales-summary-card-label">Média</span>
     </div>
-  )
+  );
 }
 
-export default salesSummaryCard
+export default SalesSummaryCard;
